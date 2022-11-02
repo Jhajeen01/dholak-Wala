fb6710070c2734c308478ffd3e862d35c93f6582
Gaurav Jha <59960697+Jhajeen01@users.noreply.github.com>
2022-11-03 02:08:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+
+var totalNumOfDrums=document.querySelectorAll(".drum").length;
+for (var i = 0; i < totalNumOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        
+        var b= this.innerHTML;
+
+        switch (b) {
+            case "a":
+                var kick=new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            case "w":
+                var crash=new Audio("sounds/crash.mp3");
+                console.log("hi");
+                crash.play();
+                break;
+            case "s":
+                var snare= new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "d":
+                var tom1= new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var tom2= new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "k":
+                var tom3=new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "l":
+                var tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            default:
+                console.log(b);
+        }
+
+
+        // alert("clicked");
+        // audio.play();
+    });
+     
+}
+// alert(totalNumOfDrums); 
+
+
+//for audio
+// var audio=new Audio('sounds/tom-1.mp3');
+
+
+// for(i=0;i<totalNumOfDrums;i++)
+// {
+//     document.querySelectorAll(".drum").innerHTML()//notWorking
+// }
